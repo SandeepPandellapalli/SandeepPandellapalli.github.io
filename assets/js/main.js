759f2318483b31ab858ac7482287e4111ddd7f56
Prabhakara Sai Sandeep Pandellapalli <sandeep.pandellapalli@gmail.com>
2025-03-07 04:52:35 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -51,7 +51,7 @@ const sr = ScrollReveal({
 //     reset: true
 });
 
-sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text,skills__container, skills__list,skill__button ',{}); 
+sr.reveal('.home__data, .about__img, .skills__category',{}); 
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
 sr.reveal('.home__social-icon, .work__title' ,{ interval: 200}); 
-sr.reveal('.skills__data, .work__img, .contact__input, skills__container, skills__list,skill__button',{interval: 200}); 
+sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
